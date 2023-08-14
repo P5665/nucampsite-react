9c5a6d8bd43fec84f7a455820f56bbb6e835ae1e
Ben Peterson <benp5665@gmail.com>
2023-08-13 19:21:30 -0700

    AnimatedDisplayCard with react spring

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,5 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
@@ -9,11 +9,14 @@ const DisplayList = () => {
 
 	return (
 		<Row>
-			{items.map((item, index) => (
-				<Col className='m-1 d-flex align-items-stretch' key={index} md>
-					<DisplayCard item={item} />
-				</Col>
-			))}
+			{items.map(
+				(item, index) =>
+					item && (
+						<Col className='m-1 d-flex align-items-stretch' key={index} md>
+							<AnimatedDisplayCard item={item} className />
+						</Col>
+					)
+			)}
 		</Row>
 	);
 };
